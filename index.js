e769a376a6ccbc25f7aab339586c6af60b668c05
pruebasdecodigos <pruebasparatd@gmail.com>
2025-09-05 23:51:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
-   document.addEventListener("DOMContentLoaded", () => {
-    const skills = document.querySelectorAll(".progress-bar span");
-    const texts = document.querySelectorAll(".progress-text");
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".skill-card");
 
-    skills.forEach((bar, i) => {
-      let finalValue = bar.getAttribute("data-progress");
-      let value = 0;
-
-      // animar barra
-      let anim = setInterval(() => {
-        if (value >= finalValue) {
-          clearInterval(anim);
-        } else {
-          value++;
-          bar.style.width = value + "%";
-          texts[i].textContent = value + "%";
-        }
-      }, 20);
+  const observer = new IntersectionObserver((entries, obs) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show"); 
+        obs.unobserve(entry.target); // comentario echo para saber que esta etiqueta se puede quitar si se quiere que cada vez que se haga scroll se vuelva a activar la animación
+      }
     });
-  });
+  }, { threshold: 0.2 }); 
 
+  cards.forEach(card => observer.observe(card));
+});
+
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      observer.unobserve(entry.target); // se activa solo una vez
+    }
+  });
+}, { threshold: 0.2 });
+
+hiddenElements.forEach(el => observer.observe(el));
+
+
+
+//asdad
+
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");  // abre/cierra menú
+        hamburger.classList.toggle("toggle"); // anima la X
+    });
